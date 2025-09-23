6214a1f2924e96f8072dca11041360f90c1ddb5b
Dwg8204 <duongba0802@gmail.com>
2025-09-23 14:15:24 +0700

    Add API test publish MQTT (deviceled)

--- backend/controllers/actionHistory.controller.js
+++ backend/controllers/actionHistory.controller.js
@@ -1,3 +1,4 @@
+const MqttClient  = require('../config/mqtt');
 const actionHistory = require('../models/actionHistory.model');
 
 module.exports.index = async (req, res) => {
@@ -24,4 +25,49 @@ module.exports.index = async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
+};
+
+module.exports.createAction = async (req, res) => {
+  try {
+    const {device, action} = req.body;
+    const newAction = new actionHistory({
+      device, action
+    });
+    await newAction.save();
+    const message = JSON.stringify({device, action});
+    MqttClient.publish('deviced', message);
+    console.log("MQTT pub: ", message);
+    res.status(201).json(newAction);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports.testMqtt = async (req, res) => {
+  try {
+    const {device, action} = req.body;
+    // console.log(req.body);
+    if (!device || !action) {
+      return res.status(400).json({ message: "Thiếu device hoặc action" });
+
+    }
+    const publishMessage = (device, action) => {
+      return new Promise((resolve, reject) => {
+        MqttClient.publish('deviced', JSON.stringify({ device, action }), (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    };
+    await publishMessage(device, action);
+    console.log("MQTT pub: ", device, action);
+    res.status(200).json({ message: "Đã gửi topic qua MQTT" });
+  } catch (error) {
+    console.log("Lỗi MQTT pub: ", error);
+    res.status(500).json({ message: error.message });
+  }
+
 };
--- backend/routes/actionHistory.route.js
+++ backend/routes/actionHistory.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/actionHistory.controller');
 
 router.get('/', controller.index);
-
+router.post('/create', controller.createAction);
+router.post('/test-mqtt', controller.testMqtt);
 module.exports = router;
